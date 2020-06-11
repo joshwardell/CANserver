694ba8af6d0f1dbb9c88f2bdac17db6eff04f8a3
iChris93 <66568878+iChris93@users.noreply.github.com>
2020-06-11 08:05:15 -0400

    Update hexID.m

--- DBCtoCSV/hexID.m
+++ DBCtoCSV/hexID.m
@@ -8,6 +8,6 @@ function [hexIDString] = hexID(ID) %function decleration
 elseif numel(num2str(dec2hex(str2num(ID)))) < 3 %if the hex is only two digits
     hexIDString = strcat('0x0',num2str(dec2hex(str2num(ID)))); %add 0x0
 else %otherwise
-    hexIDString = strcat('0x',num2str(dec2hex(str2num(ID)))); %ad 0x
+    hexIDString = strcat('0x',num2str(dec2hex(str2num(ID)))); %add 0x
 end
 endfunction
