3fb65f0011e08970c29698af07407a30b11cd074
iChris93 <66568878+iChris93@users.noreply.github.com>
2020-06-11 08:04:54 -0400

    Create hexID.m

---
+++ DBCtoCSV/hexID.m
@@ -0,0 +1,13 @@
+%convertStringValue to hex
+%Chris Allemang Jun 10 2020
+%To be used with DBCtoCSV.m
+
+function [hexIDString] = hexID(ID) %function decleration 
+  if numel(num2str(dec2hex(str2num(ID)))) < 2 %if the hex is only one digit
+    hexIDString = strcat('0x00',num2str(dec2hex(str2num(ID)))); %add 0x00 to start
+elseif numel(num2str(dec2hex(str2num(ID)))) < 3 %if the hex is only two digits
+    hexIDString = strcat('0x0',num2str(dec2hex(str2num(ID)))); %add 0x0
+else %otherwise
+    hexIDString = strcat('0x',num2str(dec2hex(str2num(ID)))); %ad 0x
+end
+endfunction
