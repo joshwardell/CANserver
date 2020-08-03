945d147689a8b78c7bbc537ec7bf2bc7778796ea
Chris Whiteford <github@chrisandtennille.com>
2020-08-03 09:10:17 -0400

    Updating devserver to reflect the changes to the system

--- ui-devserver/index.js
+++ ui-devserver/index.js
@@ -11,14 +11,6 @@ const app = express();
 const dataDir = path.join(__dirname, '../data');
 
 // mock objects
-const config =  {
-    displaysettings: {
-        disp0: "65535c${BattPower}vWK  Bu${BattPower_Scaled_Bar}b0m100r",
-        disp1: "65535c${RearTorque}vMNu${RearTorque_Scaled_Bar}b0m100r",
-        disp2: "$if BSR {{2v63488c6m100r}} $elseif BSL {{1v63488c6m100r}} $else {{65535c${VehSpeed}v${SpeedUnitString}u${BattPower_Scaled_Bar}b0m100r}}",
-        disp3: "1m2s DISPLAY    3   t500r",
-    }
-};
 
 const network =  {
     networksettings: {
@@ -90,31 +82,23 @@ const analysis_update_json = {
 };
 
 const debug_update_json = {
-    vehiclestatus: {
-      BattVolts: 350,
-      BattAmps: 99,
-      RearTorque: 1210,
-      FrontTorque: 0,
-      MinBattTemp: 0,
-      BattCoolantRate: 0,
-      PTCoolantRate: 0,
-      MaxRegen: 45,
-      MaxDisChg: 0,
-      VehSpeed: 69,
-      SpeedUnit: 0,
-      v12v261: 0,
-      BattCoolantTemp: 0,
-      PTCoolantTemp: 0,
-      BattRemainKWh: 0,
-      BattFullKWh: 420,
-      InvHStemp376: 0,
-      BSR: 0,
-      BSL: 0,
-      DisplayOn: 1
+    dynamicanalysisitems:{
+        BattAmps:0,
+        BattCoolantRate:0,
+        BattVolts:0,
+        DisplayOn:0,
+        DistanceUnitMiles:0,
+        MinBattTemp:0,
+        RearTorque:0,
+        VehSpeed:0
     },
-    dynamicanalysisitems: {
-      TestVolts: 350,
-      TestAmps: 99
+    processeditems:{
+        BSL:0,
+        BSR:0,
+        BattPower:0,
+        BattPower_Scaled_Bar:0,
+        RearTorque_Scaled_Bar:0,
+        VehSpeed:0
     }
 };
 
@@ -123,8 +107,8 @@ app.get('/', (req, res) => {
     res.sendFile(dataDir + '/html/index.html');
 });
 
-app.get('/config', (req, res) => {
-    res.sendFile(dataDir + '/html/config.html');
+app.get('/displays', (req, res) => {
+    res.sendFile(dataDir + '/html/displays.html');
 });
 
 app.get('/network', (req, res) => {
@@ -143,14 +127,35 @@ app.get('/analysis', (req, res) => {
     res.sendFile(dataDir + '/html/analysis.html');
 });
 
-// web service endpoints
-app.get('/config_update', (req, res) => {
-    res.json(config);
+app.get('/processing', (req, res) => {
+    res.sendFile(dataDir + '/html/processing.html');
 });
 
-app.post('/config_save', (req, res) => {
-    res.redirect('/config');
+
+
+
+// web service endpoints
+app.get('/display_load', (req, res) => {
+    res.send('return "65535c" .. math.floor(CANServer_getVar("BattPower") * 10) .. "vWK  Bu" .. math.floor(CANServer_getVar("BattPower_Scaled_Bar")) .. "b0m100r"');
 });
+app.get('/display_stats', (req, res) => {
+    res.json({
+        state:true,
+        errorstring:"",
+        mean:0.9,
+        mode:1,
+        max:1,
+        min:0,
+        stddev:0.3
+    });
+});
+app.get('/display_save', (req, res) => {
+    res.status(200).end()
+});
+
+
+
+
 
 app.get('/network_update', (req, res) => {
     res.json(network);
@@ -164,6 +169,10 @@ app.get('/network_stationlist', (req, res) => {
     res.json(network_stationlist_json);
 });
 
+
+
+
+
 app.get('/debug_update', (req, res) => {
     res.json(debug_update_json);
 });
@@ -171,6 +180,10 @@ app.get('/debug_update', (req, res) => {
 app.post('/debug_save', (req, res) => {
     res.redirect('/debug');
 });
+
+
+
+
 
 app.get('/logs_update', (req, res) => {
     res.json(logs);
@@ -188,6 +201,10 @@ app.post('/logs_save', (req, res) => {
     res.redirect('/logs');
 });
 
+
+
+
+
 app.get('/analysis_load', (req, res) => {
     res.json(analysis_load_json);
 });
@@ -195,6 +212,35 @@ app.get('/analysis_load', (req, res) => {
 app.get('/analysis_update', (req, res) => {
     res.json(analysis_update_json);
 });
+
+
+
+app.get('/processing_script', (req, res) => {
+    res.send("\
+--[[ Sort out the wattage based on V*A --]]\n\
+local battPower = (CANServer_getVar(\"BattVolts\") * CANServer_getVar(\"BattAmps\") / 1000.0);\n\
+CANServer_setVar(\"BattPower\", battPower);\n\
+\n\
+--[[ Scale the battery power for the bargraph --]]\n\
+local scaledBattPower = math.min(math.max((24) * (battPower) / (300), -24), 24);\n\
+CANServer_setVar(\"BattPower_Scaled_Bar\", math.floor(scaledBattPower + 0.5));\n\
+"
+    );
+});
+
+app.get('/processing_stats', (req, res) => {
+    res.json({
+        state:true,
+        errorstring:"",
+        mean:0.3,
+        mode:0,
+        max:2,
+        min:0,
+        stddev:0.640313
+    });
+});
+
+
 
 // fallback other assets like CSS and JS
 app.use(express.static('../data'));
