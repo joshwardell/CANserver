7b59fc1bf488f11e68cc7b01ef89fb266a50bedc
Chris Whiteford <github@chrisandtennille.com>
2020-08-04 17:13:06 -0400

    This is a manual merge of the PR that @andreasrs submitted.
    I have used the bulk of the code from that PR and then modified things based on some changes that happened last minute to the code base.
    
    One place where I have diverged is that I have kept area specific code in their respective pages.  Common functions are now in app.js though.
    
    There is no longer a dependency on the zepto library.  Its all straight js.

--- data/js/app.js
+++ data/js/app.js
@@ -1,43 +1,54 @@
 //https://github.com/MatheusAvellar/textarea-line-numbers
 const TLN={eventList:{},update_line_numbers:function(e,t){let n=e.value.split("\n").length-t.children.length;if(n>0){const e=document.createDocumentFragment();for(;n>0;){const t=document.createElement("span");t.className="tln-line",e.appendChild(t),n--}t.appendChild(e)}for(;n<0;)t.removeChild(t.lastChild),n++},append_line_numbers:function(e){const t=document.getElementById(e);if(null==t)return console.warn("[tln.js] Couldn't find textarea of id '"+e+"'");if(-1!=t.className.indexOf("tln-active"))return console.warn("[tln.js] textarea of id '"+e+"' is already numbered");t.classList.add("tln-active"),t.style={};const n=document.createElement("div");n.className="tln-wrapper",t.parentNode.insertBefore(n,t),TLN.update_line_numbers(t,n),TLN.eventList[e]=[];const l=["propertychange","input","keydown","keyup"],o=function(e,t){return function(n){(10!=+e.scrollLeft||37!=n.keyCode&&37!=n.which&&"ArrowLeft"!=n.code&&"ArrowLeft"!=n.key)&&36!=n.keyCode&&36!=n.which&&"Home"!=n.code&&"Home"!=n.key&&13!=n.keyCode&&13!=n.which&&"Enter"!=n.code&&"Enter"!=n.key&&"NumpadEnter"!=n.code||(e.scrollLeft=0),TLN.update_line_numbers(e,t)}}(t,n);for(let n=l.length-1;n>=0;n--)t.addEventListener(l[n],o),TLN.eventList[e].push({evt:l[n],hdlr:o});const r=["change","mousewheel","scroll"],s=function(e,t){return function(){t.scrollTop=e.scrollTop}}(t,n);for(let n=r.length-1;n>=0;n--)t.addEventListener(r[n],s),TLN.eventList[e].push({evt:r[n],hdlr:s})},remove_line_numbers:function(e){const t=document.getElementById(e);if(null==t)return console.warn("[tln.js] Couldn't find textarea of id '"+e+"'");if(-1==t.className.indexOf("tln-active"))return console.warn("[tln.js] textarea of id '"+e+"' isn't numbered");t.classList.remove("tln-active");const n=t.previousSibling;if("tln-wrapper"==n.className&&n.remove(),TLN.eventList[e]){for(let n=TLN.eventList[e].length-1;n>=0;n--){const l=TLN.eventList[e][n];t.removeEventListener(l.evt,l.hdlr)}delete TLN.eventList[e]}}};
 
-var navLinks = [
-  {name: "Status", url: "/"},
-  {name: "Displays", url: "/displays"},
-  {name: "Network", url: "/network"},
-  {name: "Analysis", url: "/analysis"},
-  {name: "Logging", url: "/logs"},
-]
-        
-$ulObj = $('<ul>');
-Object.keys(navLinks).forEach((k, i) => {
-  $liObj = $('<li>');
-  if (window.location.pathname == navLinks[k].url) {
-    $liObj.addClass("active");
-  }
-  $aObj = $('<a>');
-  $aObj.html(navLinks[k].name);
-  $aObj.prop('href', navLinks[k].url);
-  $liObj.append($aObj);
-  $ulObj.append($liObj);
-});
 
-$navObj = $('<nav>');
-$navObj.append($ulObj);
-$navObj.insertBefore('main');
+//Nav and footer 
+const setupNav = () => {
+  var navLinks = [
+    {name: "Status", url: "/"},
+    {name: "Displays", url: "/displays"},
+    {name: "Network", url: "/network"},
+    {name: "Analysis", url: "/analysis"},
+    {name: "Logging", url: "/logs"},
+  ]
+          
+  //Build the NAV
+  ulObj = el('ul');
+  
+  Object.keys(navLinks).forEach((k, i) => {
+    liObj = el('li');
+    if (window.location.pathname == navLinks[k].url) {
+      addClass(liObj, "active");
+    }
+    aObj = el('a');
+    aObj.innerText = navLinks[k].name;
+    aObj.href = navLinks[k].url;
+    liObj.appendChild(aObj);
+    ulObj.appendChild(liObj);
+  });
+  
+  navObj = el('nav');
+  navObj.appendChild(ulObj);
+  
+  (document.getElementsByTagName('body')[0]).insertBefore(navObj, document.getElementsByTagName('main')[0]);
+  
+  //Build the Footer
+  footerObj = el('footer');
+  footerObj.innerHTML = "<span>\
+      Github: <a href=\"https://github.com/joshwardell/CANserver\">https://github.com/joshwardell/CANserver</a>\
+    </span>\
+    <span>\
+      <label for=\"theme\">Visual theme:</label>\
+      <select name=\"theme\" id=\"theme\">\
+        <option value=\"business\">Business</option>\
+        <option value=\"pastel\">Pastel</option>\
+      </select> \
+    </span>";
+  (document.getElementsByTagName('body')[0]).appendChild(footerObj);
+}
 
-$('<footer>\
-      <span>\
-        Github: <a href="https://github.com/joshwardell/CANserver">https://github.com/joshwardell/CANserver</a>\
-      </span>\
-      <span>\
-        <label for="theme">Visual theme:</label>\
-          <select name="theme" id="theme">\
-            <option value="business">Business</option>\
-            <option value="pastel">Pastel</option>\
-          </select> \
-        </span>\
-    </footer>').insertAfter('main');
+document.addEventListener('DOMContentLoaded', setupNav);
+    
     
     
 // layout theme selector
@@ -75,7 +86,7 @@ const themeSelectEl = document.getElementById('theme');
 if (themeSelectEl) {
     // activate persisted choice
     const persistedSelect = localStorage.getItem('theme') || 'business';
-    activateColorTheme(persistedSelect)
+    activateColorTheme(persistedSelect);
     themeSelectEl.value = persistedSelect;
 
     // activate and persist new theme choice
@@ -84,4 +95,100 @@ if (themeSelectEl) {
         activateColorTheme(name);
         localStorage.setItem('theme', name);
     };
+}
+
+
+//Data fetching utils
+const getText = url => fetch(url).then(r => r.text()).catch(console.error);
+const getJSON = url => fetch(url).then(r => r.json()).catch(console.error);
+const postData = (url, data) => fetch(url, { method: 'POST', body: data });
+
+// dom utils
+const el = (type, initial = {}) => {
+    const element = document.createElement(type);
+
+    if (initial.id) {
+      element.id = initial.id;
+    }
+
+    if (initial.inner) {
+        element.innerText = initial.inner;
+    }
+
+    if (initial.children) {
+        initial.children.forEach(child => element.appendChild(child));
+    }
+
+    if (initial.className) {
+        element.className = initial.className;
+    }
+
+    if (initial.attributes) {
+        Object.keys(initial.attributes).forEach(attr => {
+            element.setAttribute(attr, initial.attributes[attr]);
+        })
+    }
+
+    return element;
+}
+
+const addClass = (element, classname) => element && element.classList && element.classList.add(classname);
+const removeClass = (element, classname) => element && element.classList && element.classList.remove(classname);
+
+const show = element => removeClass(element, 'hidden');
+const hide = element => addClass(element, 'hidden');
+
+const good = statusEl => {
+    statusEl.innerText = 'Good';
+    statusEl.classList.add('good');
+    statusEl.classList.remove('bad');
+};
+
+const bad = statusEl => {
+    statusEl.innerText = 'Bad';
+    statusEl.classList.add('bad');
+    statusEl.classList.remove('good');
+};
+
+const keyEventWithGracefulTabs = (textarea, e) => {
+  const keyCode = e.keyCode || e.which;
+
+  if (keyCode == 9) {
+    e.preventDefault();
+
+    var start = textarea.selectionStart;
+    var end = textarea.selectionEnd;
+
+    // set textarea value to: text before caret + tab + text after caret
+    textarea.value = (textarea.value.substring(0, start)
+        + "\t"
+        + textarea.value.substring(end));
+
+    // put caret at right position again
+    textarea.selectionStart = textarea.selectionEnd = start + 1;
+  }
+}
+
+// data conversion
+const hexToDec = hex => {
+  let result = 0;
+  hex = hex.toLowerCase();
+
+  for (var i = 0; i < hex.length; i++) {
+    const digitValue = '0123456789abcdefgh'.indexOf(hex[i]);
+    result = result * 16 + digitValue;
+  }
+
+  return result;
+}
+
+const decToHex = (dec, padding) => {
+  var hex = Number(dec).toString(16).toUpperCase();
+  padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
+
+  while (hex.length < padding) {
+    hex = "0" + hex;
+  }
+
+  return hex;
 }
