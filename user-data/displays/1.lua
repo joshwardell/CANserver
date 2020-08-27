a4842301a9f0a4532a8ee2eaf604e6a453be2bb3
Josh Wardell <jwardell@gmail.com>
2020-08-26 21:54:56 -0400

    fixed units

--- user-data/displays/1.lua
+++ user-data/displays/1.lua
@@ -5,4 +5,4 @@ end
 local rearTorque = CANServer_getAnalysisVar("RearTorque")
 local graphRearTorque = math.floor(math.min(math.max((24) * (rearTorque) / (400), -24), 24))
 
-return "65535c" .. math.floor(rearTorque * 10) .. "vWK  Bu" .. graphRearTorque .. "b0m100r"
+return "65535c" .. math.floor(rearTorque * 10) .. "vMNu" .. graphRearTorque .. "b0m100r"
