fb76b9d9efa2a338a8510d62c6befc56b10a667d
Josh Wardell <jwardell@gmail.com>
2020-10-25 15:30:30 -0400

    Update with systemactive signal and add comments

--- user-data/displays/0.lua
+++ user-data/displays/0.lua
@@ -1,7 +1,11 @@
-if (CANServer_getAnalysisVar("DisplayOn") < 1) then
-    return "1m t0b1000r"
+-- Display battery power in KW, with battery power on bargraph
+
+-- Turn display off if car center display is off
+if ((CANServer_getAnalysisVar("DisplayOn") == 0) and (CANServer_getAnalysisVar("UI_systemActive") == 1)) then
+    return "1m                  t0b1000r"
 end
 
+-- calculate power from volts * amps
 local battPowerKW = (CANServer_getAnalysisVar("BattVolts") * CANServer_getAnalysisVar("BattAmps") / 1000.0)
 local graphBattPower = math.floor(math.min(math.max((24) * (battPowerKW) / (300), -24), 24))
 
