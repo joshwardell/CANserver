338772d93fd39d1181ac16b11a84345bb8b03a3a
Josh Wardell <jwardell@gmail.com>
2020-10-25 15:36:10 -0400

    Add systemactive signal and comments

--- user-data/displays/2.lua
+++ user-data/displays/2.lua
@@ -1,14 +1,18 @@
-if (CANServer_getAnalysisVar("DisplayOn") < 1) then
-    return "1m t0b1000r"
+-- Display speed, with blindspot overlay arrows
+
+-- Turn display off if car center display is off
+if ((CANServer_getAnalysisVar("DisplayOn") == 0) and (CANServer_getAnalysisVar("UI_systemActive") == 1)) then
+    return "1m                  t0b1000r"
 end
 
+-- Calculate battery power for bargraph
 local battPowerKW = (CANServer_getAnalysisVar("BattVolts") * CANServer_getAnalysisVar("BattAmps") / 1000.0)
 local graphBattPower = math.floor(math.min(math.max((24) * (battPowerKW) / (300), -24), 24))
 
+-- Decode right and left blindspot signals to send to display
 local bsr = CANServer_getAnalysisVar("BSR");
 local bsl = CANServer_getAnalysisVar("BSL");
 local bsd = 0;
-
 if (bsl == 1 or bsl == 2) then
     bsd = 1;
 end
@@ -20,7 +24,7 @@ if (bsd > 0) then
 else 
 	local speedUnitText = "HMK"
 	local displaySpeed = CANServer_getAnalysisVar("VehSpeed")
-	if (CANServer_getAnalysisVar("DistanceUnitMiles") == 1) then
+	if (CANServer_getAnalysisVar("DistanceUnitMiles") == 1) then -- convert to MPH
 		speedUnitText = "HPM"
 		displaySpeed = displaySpeed * 0.6213712;
 	end
